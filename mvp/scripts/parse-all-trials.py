91a0820960d363c495c648f7024be57503da79af
Carlos Martinez de la Serna <cmserna@Carloss-MacBook-Pro.local>
2018-10-07 10:38:24 -0400

    script for parsing all xml files and exporting data as json

---
+++ mvp/scripts/parse-all-trials.py
@@ -0,0 +1,163 @@
+'''
+Clinical Trials: Parse all XML files and save data in a json file
+'''
+
+import json
+import os
+import xml.etree.ElementTree as ET
+import shutil
+
+
+'''
+Create paths and folders
+'''
+
+# Folders with xml files
+path_to_all_xml_trials = os.path.abspath('../data/all_trials/')
+
+# Test folder ~100 files
+path_to_test_folder = os.path.abspath('../data/test/')
+
+# Remove old files from test folder
+shutil.rmtree(path_to_test_folder)
+
+# Path for json folder
+path_to_json_file = os.path.abspath('../data/json/')
+
+# Variable for all parsed files
+all_parsed_files = []
+
+print('\n-------------------------\n')
+print('\nPaths and folders created\n')
+
+
+# Create folders
+create_folders_errors = []
+
+
+def create_folders(paths=[]):
+    for p in paths:
+        try:
+            os.mkdir(p)
+            if p:
+                print('{} created'.format(p))
+        except IOError as e:
+            create_folders_errors.append(e)
+            pass
+
+        print('\n-------------------------\n')
+        print('\nFolders created. Except: {}\n'.format(create_folders_errors))
+
+
+all_folders = [path_to_all_xml_trials, path_to_test_folder, path_to_json_file]
+
+# Run create folders function
+create_folders(all_folders)
+
+
+'''
+Parse all xml files and save them in all_parsed_files.
+Slow. Checks for xml files
+'''
+
+
+def parse_xml_files(path_to_folder):
+    for filename in os.listdir(path_to_folder):
+        if not filename.endswith('.xml'): continue
+        fullname = os.path.join(path_to_folder, filename)
+        all_parsed_files.append(ET.parse(fullname).getroot())
+
+        # Checking message to the terminal
+        print("{} file parsed\n".format(fullname))
+
+    print('Number of files parsed: {}\n'.format(len(all_parsed_files)))
+
+    return all_parsed_files
+
+
+# Run parse_xml_files function
+all_parsed_files.clear()
+parse_xml_files(path_to_all_xml_trials)
+
+
+'''
+Find values by unique tags in XML files
+Save them in a dictionary
+Export them to a json file
+'''
+
+all_data_dictionary = {}
+print('\n-------------------------\n')
+print('\nCreated al_data_dictionary\n')
+
+def create_dictionary_from_tag(tag, parsed_files = all_parsed_files, name_dictionary = all_data_dictionary):
+
+    # Variable to store values
+    # keys = []
+    values = []
+
+    # Iterate through all xml parsed files and tags, and append data to values
+    for n in parsed_files:
+        for i in n.iter(tag):
+            if i.text != 0:
+                values.append(i.text)
+            else:
+                values.append('nan')
+            print("{} file parsed\n".format(i.text))
+
+    # Create dictionary and set tags as keys
+    name_dictionary.setdefault(tag, [])
+
+    # Append values to dictionary
+    for i in values:
+        name_dictionary[tag].append(i)
+        print("{} file added to the dictionary\n".format(i))
+
+
+# Execute function only with unique values
+create_dictionary_from_tag('nct_id')
+create_dictionary_from_tag('study_first_submitted')
+create_dictionary_from_tag('source')
+create_dictionary_from_tag('brief_title')
+
+print('\n-----------------\n')
+print('Dictionary created')
+
+
+# Append first condition to dictionary
+# Add Nan for missing values
+
+all_data_dictionary.setdefault('condition', [])
+
+for n in all_parsed_files:
+        value_conditions = n.find('condition')
+        if n.find('condition') is not None:
+            all_data_dictionary['condition'].append(value_conditions.text)
+        else:
+            all_data_dictionary['condition'].append('None')
+
+
+# Check number of values by key
+for key, value in all_data_dictionary.items():
+    print(key, len(list(filter(bool, value))))
+
+
+'''
+Export dictionary to json file
+'''
+
+# Final json file
+json_file = '/all_trials_json'
+
+print('\n-------------------------\n')
+print('\nCreated path to json file\n')
+
+
+# Dump dictionary into a JSON file
+with open('{}{}.json'.format(path_to_json_file, json_file), 'w') as fp:
+    json.dump(all_data_dictionary, fp)
+    print('JSON file created\n')
+
+    fl = path_to_json_file + json_file
+    json_size = round(os.path.getsize(fl + '.json') / 1000000, 2)
+    print("JSON file: {} Mb".format(json_size))
