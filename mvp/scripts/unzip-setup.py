23fd38771edb560181ee34449adeb21ecc667748
Carlos Martinez de la Serna <cmserna@Carloss-MacBook-Pro.local>
2018-12-16 15:43:30 -0500

    updating script for data download and setup

--- mvp/scripts/unzip-setup.py
+++ mvp/scripts/unzip-setup.py
@@ -16,24 +16,19 @@ print('\n-- Printing messages to terminal --\n')
 SETUP FOLDERS AND PATHS FOR all_xml_files
 
 '''
-
-# all_xml_files url
-file_to_download = 'https://clinicaltrials.gov/AllPublicXML.zip'
-
-# Paths for folders to store all_xml_files
-
-path_zip_file = os.path.abspath('../data/zip')
-
-path_for_download = os.path.abspath('../data/zip/AllPublicXML.zip')
-
-path_dest_unzip = os.path.abspath('../data/unzip/')
-
-path_all_trials = os.path.abspath('../data/all_trials/')
-
-
 # Create folders
 
 def create_folders(paths=[]):
+    # all_xml_files url
+    file_to_download = 'https://clinicaltrials.gov/AllPublicXML.zip'
+
+    # Paths for folders to store all_xml_files
+
+    path_zip_file = os.path.abspath('../data/zip')
+    path_for_download = os.path.abspath('../data/zip/AllPublicXML.zip')
+    path_dest_unzip = os.path.abspath('../data/unzip/')
+    path_all_trials = os.path.abspath('../data/all_trials/')
+
     for p in paths:
         try:
             os.mkdir(p)
@@ -45,119 +40,117 @@ def create_folders(paths=[]):
 
     print('\nSetup folders created\n')
 
-
-all_folders = [path_zip_file, path_for_download, path_dest_unzip, path_all_trials]
-
-
-create_folders(all_folders)
-
+    all_folders = [path_zip_file, path_for_download, path_dest_unzip, path_all_trials]
 
 '''
 SAVE ZIP FILE FROM CLINICALTRIALS.GOV IN THE ZIP FOLDER
 
 '''
 
-print('Beginning file download from clinicaltrials.gov\n')
+def save_zip_file():
+    print('Beginning file download from clinicaltrials.gov\n')
+    url = 'https://clinicaltrials.gov/AllPublicXML.zip'
+    wget.download(url, path_for_download)
 
-url = 'https://clinicaltrials.gov/AllPublicXML.zip'
+    now = datetime.datetime.now()
+    date_format = now.strftime("%Y-%m-%d %H:%M")
+    current_download = ('\nall_xml_files downloaded on {}\n'.format(date_format))
 
-wget.download(url, path_for_download)
-
-now = datetime.datetime.now()
-date_format = now.strftime("%Y-%m-%d %H:%M")
-current_download = ('\nall_xml_files downloaded on {}\n'.format(date_format))
-
-print(current_download)
+    print(current_download)
 
 
 '''
 UNZIP BULF DOWNLOAD
 
 '''
-
-bulk_file = '/AllPublicXML.zip'
-
-
 def unzip_file(f, dest):
+    bulk_file = '/AllPublicXML.zip'
     print("Unzipping file...\n")
     all_xml_files_zip = zipfile.ZipFile(f)
     all_xml_files_zip.extractall(dest)
     print('\n----{} unzipped in:\n{}----\n'.format(f, dest))
 
-
-unzip_file(path_for_download + bulk_file, path_dest_unzip)
-
-
 '''
 Basic checks on files
 '''
-
 # Number of folders
 
-folders = []
+def check_files():
+    folders = []
 
-for i in os.listdir(path_dest_unzip):
-    folders.append(i)
+    for i in os.listdir(path_dest_unzip):
+        folders.append(i)
 
-print("\nNumber of folders: {}\n".format(folders))
-print("\nFirst 5 folders: {}\n".format(folders[0:5]))
+    print("\nNumber of folders: {}\n".format(folders))
+    print("\nFirst 5 folders: {}\n".format(folders[0:5]))
 
 
 # Calculate number of trials in unzipped file
 
-def list_files(dir):
-    r = []
-    for root, dirs, files in os.walk(dir):
-        for name in files:
-            r.append(os.path.join(root, name))
-    return r
+def calculate_number_trials():
+    def list_files(dir):
+        r = []
+        for root, dirs, files in os.walk(dir):
+            for name in files:
+                r.append(os.path.join(root, name))
+        return r
 
 
-all_xml_files = list_files(path_dest_unzip)
+    all_xml_files = list_files(path_dest_unzip)
 
-print("\nNumber of trials: {}".format() + str(len(all_xml_files) - 1))
-
+    print("\nNumber of trials: {}".format() + str(len(all_xml_files) - 1))
 
 '''
 MOVE ALL XML FILES INTO A SINGLE FOLDER
 
 '''
+def move_xml_files():
+    for d in all_xml_files:
+        try:
+            if d.endswith('.xml'):
+                shutil.move(d, path_all_trials)
+        except IOError as e:
+            print(e)
+            pass
 
-for d in all_xml_files:
-    try:
-        if d.endswith('.xml'):
-            shutil.move(d, path_all_trials)
-    except IOError as e:
-        print(e)
-        pass
-
-print("\nFiles moved to {}".format(path_all_trials))
-
+    print("\nFiles moved to {}".format(path_all_trials))
 
 # Get all_trials numbers & size
 
-records = []
+
+def get_trials_size():
+    records = []
+    def get_size(start_path):
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(start_path):
+            for f in filenames:
+                records.append(f)
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        return total_size / 1000000000
 
 
-def get_size(start_path):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            records.append(f)
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size / 1000000000
-
-
-print("\nAll_Trials folder: " + str(round(get_size(path_all_trials), 2)) + " Gb")
-print(records[0:5])
+    print("\nAll_Trials folder: " + str(round(get_size(path_all_trials), 2)) + " Gb")
+    print(records[0:5])
 
 
 # Remove unzip files after completing setup
 
-try:
-    shutil.rmtree(path_dest_unzip)
-    print("\nunzip folder deleted")
-except IOError as e:
-    print(e)
-    pass
+def remove_extra_folders():
+    try:
+        shutil.rmtree(path_dest_unzip)
+        print("\nunzip folder deleted")
+    except IOError as e:
+        print(e)
+        pass
+
+
+#RUN SCRIPT
+create_folders(all_folders)
+save_zip_file()
+unzip_file(path_for_download + bulk_file, path_dest_unzip)
+check_files()
+calculate_number_trials()
+move_xml_files()
+get_trials_size()
+remove_extra_folders()
