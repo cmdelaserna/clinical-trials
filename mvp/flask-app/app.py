d370cb7d98657d96f4a46ba65efbb6a59a0eb40a
Carlos Martinez de la Serna <cmserna@Carloss-MacBook-Pro.local>
2019-06-17 14:47:34 -0400

    testing simple search form

--- mvp/flask-app/app.py
+++ mvp/flask-app/app.py
@@ -4,9 +4,10 @@
 #Configuration
 #
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 import pandas as pd
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -25,7 +26,20 @@ def index():
 	df = pd.read_sql_query("SELECT * FROM all_trials LIMIT 1", conn)
 	return render_template('index.html', data = df.to_html())
 
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
 
+      # conn = sqlite3.connect(DATABASE)
+
+      # first_sql = 'SELECT * FROM all_trials WHERE all_text LIKE '
+      # result_value = request.form['Search']
+      # query_string =   first_sql + str(result_value) + ' LIMIT 1'
+
+      # df = pd.read_sql_query(query_string, conn)
+
+      return render_template("result.html",result = result)
 
 if __name__ == "__main__": app.run()
 
