00cf83bf9e5d1adbd008d160b852703af26d4ceb
CARLOS MARTINEZ DE LA SERNA <carlosmartinezserna@gmail.com>
2019-06-18 21:44:27 -0400

    added python function to create timeline json data

--- mvp/flask-app/app.py
+++ mvp/flask-app/app.py
@@ -4,54 +4,68 @@
 #Configuration
 #
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 import sqlite3
 import pandas as pd
 import json
 import time
 
+'''
+CONFIGURATION
+'''
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 DATABASE = '../data/working_data/database.db'
 
-#Views
 
+'''
+FUNCTIONS
+'''
+
+
+# Views
+
+# Index
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index')
 def index():
    return render_template('index.html')
 
-
+# Results
 @app.route('/result', methods = ['GET', 'POST'])
 def search():
    if request.method == 'POST':
       search = request.form
 
+      #Connect to db
       conn = sqlite3.connect(DATABASE)
 
-      # Get result and search for string match
-      # ie: # '%breast cancer%';
+      # Search for string match
       result_value = request.form['Search']
       first_sql = "SELECT * from all_trials WHERE all_text LIKE "
       term = "'%" + str(result_value) + "%'"
-      query_string = first_sql + term
+      full_query_string = first_sql + term
 
-      # Query db and store results in df
-      df = pd.read_sql_query(query_string, conn)
+      # Query db and store results
+      df = pd.read_sql_query(full_query_string, conn)
+      # Data
       number_results = len(df)
-      df_head = df.head()
+      # Group by year
+      df_year = df.groupby(['year_submitted', 'phase']).nct_id.count()
 
       # return jsonify({'data' : df.to_json()})
 
       return render_template("result.html", 
       	data = df, 
-      	result = search, 
-      	number = number_results)
+      	search = search, 
+      	number = number_results,
+         timeline_graph = df_year.to_json())
 
    else:
       return render_template('index.html', data = None)
+
 
 
 if __name__ == "__main__": app.run()
