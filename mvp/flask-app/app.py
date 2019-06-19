dbf094d0d8a9811effee57b0ad016cc725a97d30
Carlos Martinez de la Serna <cmserna@Carloss-MacBook-Pro.local>
2019-06-19 13:40:32 -0400

    revert to model with results in independent page

--- mvp/flask-app/app.py
+++ mvp/flask-app/app.py
@@ -1,13 +1,14 @@
-#  FLASK_APP=app.py FLASK_DEBUG=1 python -m flask run
+# FLASK_APP=app.py FLASK_DEBUG=1 python -m flask run
 # To React: https://realpython.com/the-ultimate-flask-front-end/
 #
 #Configuration
 #
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 import sqlite3
 import pandas as pd
+import json
 import time
 
 '''
@@ -24,9 +25,16 @@ FUNCTIONS
 '''
 
 
-# Search
+# Views
 
+# Index
 @app.route('/', methods = ['GET', 'POST'])
+@app.route('/index')
+def index():
+   return render_template('index.html')
+
+# Results
+@app.route('/result', methods = ['GET', 'POST'])
 def search():
    if request.method == 'POST':
       search = request.form
@@ -34,20 +42,26 @@ def search():
       #Connect to db
       conn = sqlite3.connect(DATABASE)
 
-      # Search query
+      # Search for string match
       result_value = request.form['Search']
       first_sql = "SELECT * from all_trials WHERE all_text LIKE "
       term = "'%" + str(result_value) + "%'"
       full_query_string = first_sql + term
 
-      # Query db
+      # Query db and store results
       df = pd.read_sql_query(full_query_string, conn)
-
-      # Store data
+      # Data
       number_results = len(df)
+      # Group by year
       df_year = df.groupby(['year_submitted', 'phase']).nct_id.count()
 
-      return jsonify({'df_year' : df_year.to_json()}, {'number_results' : number_results})
+      # return jsonify({'data' : df.to_json()})
+
+      return render_template("result.html", 
+         data = df, 
+         search = search, 
+         number = number_results,
+         timeline_graph = df_year.to_json())
 
    else:
       return render_template('index.html', data = None)
@@ -55,4 +69,3 @@ def search():
 
 
 if __name__ == "__main__": app.run()
-
