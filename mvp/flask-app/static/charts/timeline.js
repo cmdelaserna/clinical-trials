5cd77af66293828e672373cb0a1d22391bcc1d0d
Carlos Martinez de la Serna <cmserna@Carloss-MacBook-Pro.local>
2019-11-19 17:45:26 -0500

    working on function to update data with a dropdown menu

--- mvp/flask-app/static/charts/timeline.js
+++ mvp/flask-app/static/charts/timeline.js
@@ -61,53 +61,68 @@ function responsiveChart(svg) {
     // Color scales
     // var colorOpacity = d3.scaleLinear().domain([0,maxStripe]).range([.3, 1]);
 
-    // d3 chart
-    var svgTimeline = d3.select("div#svgTimeline")
+     var svgTimeline = d3.select("div#svgTimeline")
                         .append("svg")
                         .attr('width', wTimeline)
                         .attr('height', hTimeline)
                         .call(responsiveChart);
 
-     // xAxis
-    var xAxisTimeline = d3.axisBottom()
-                        .scale(xScaletimeline);
+    // d3 chart
+    function udpateData(newData) {
+         // xAxis
+        var xAxisTimeline = d3.axisBottom()
+                            .scale(xScaletimeline);
 
-    svgTimeline.append('g')
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxisTimeline);
+        svgTimeline.append('g')
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxisTimeline);
 
 
-    var rectTimeline = svgTimeline.selectAll('rect')
-                        .data(timelineData)
-                        .enter()
-                        .append('rect');
+        var rectTimeline = svgTimeline.selectAll('rect')
+                            .data(newData)
+                            .enter()
+                            .append('rect');
 
-    var attrTimeline = rectTimeline
-                        .attr('width', wTimeline / timelineData.length - barPadding)
-                        .attr('height', height)
-                        // .attr('height', function(d) {return yScaleTimeline(d);})
-                        .attr('x', function(d, i){ return i * (wTimeline / timelineData.length);})
-                        // .attr('y', hTimeline)
-                        .attr('fill', function(d) {
-                            if (d < 1) {
-                                return gray;
-                            } else {
-                                return colorFillTimeline(d);
-                            }
-                        })
-                        .on("mouseover", function(i, d) {    
-                            div.transition()    
-                                .duration(200)    
-                                .style("opacity", .9);    
-                            div .html(i + " entries "  + " in " + timelineDataYear[d])  
-                                .style("left", (d3.event.pageX) - 10 + "px")   
-                                .style("top", (d3.event.pageY - 40) + "px")  
-                            })          
-                        .on("mouseout", function(d) {   
-                            div.transition()    
-                                .duration(500)    
-                                .style("opacity", 0); 
-                        });
-    
-    // end of chart
+        var attrTimeline = rectTimeline
+                            .attr('width', wTimeline / newData.length - barPadding)
+                            .attr('height', height)
+                            // .attr('height', function(d) {return yScaleTimeline(d);})
+                            .attr('x', function(d, i){ return i * (wTimeline / newData.length);})
+                            // .attr('y', hTimeline)
+                            .attr('fill', function(d) {
+                                if (d < 1) {
+                                    return gray;
+                                } else {
+                                    return colorFillTimeline(d);
+                                }
+                            })
+                            .on("mouseover", function(i, d) {    
+                                 div.transition()    
+                                    .duration(200)    
+                                    .style("opacity", .9);    
+                                div .html(i + " entries "  + " in " + timelineDataYear[d])  
+                                    .style("left", (d3.event.pageX) - 10 + "px")   
+                                    .style("top", (d3.event.pageY - 40) + "px")  
+                                })          
+                            .on("mouseout", function(d) {   
+                                div.transition()    
+                                    .duration(500)    
+                                    .style("opacity", 0); 
+                            });
+
+        attrTimeline.exit().remove();
+
+        // End function
+        }
+
+    udpateData(timelineData);
+
+    d3.select('#filter')
+      .on('change', function() {
+        var timelineData = eval(d3.select(this).property('value'));
+        console.log(d3.select(this).property('value'));
+        udpateData(timelineData);
+    });
+
+        
